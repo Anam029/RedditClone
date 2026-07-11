8e9e3b60627b2b15743a11ffa3bcc68691bd839e
Anam029 <your-email@gmail.com>
2026-07-12 00:34:14 +0530

    Add comment routes

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,1 +1,11 @@
 import {Router} from "express"
+import { verifyJWT } from "../middlewares/verify-auth.middlewares"
+import {CreateComment, getAllComment, updateComment, deleteComment}from "../controllers/comments.controllers"
+import { createCommunity } from "../controllers/community.controllers"
+
+const router = Router()
+
+router.post("/comment",createCommunity)
+router.get("/comment",getAllComment)
+router.patch("/comment/:commentId",updateComment)
+router.delete("/comment/:commentId",deleteComment)
