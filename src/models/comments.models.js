b31087a7730d6581cc05e11cc232ac243f05bdb3
Anam029 <your-email@gmail.com>
2026-07-11 19:46:46 +0530

    Add comment model and schema

--- src/models/comments.models.js
+++ src/models/comments.models.js
@@ -1,2 +1,21 @@
 import mongoose,{Schema} from "mongoose";
 
+const commentSchema = new Schema({
+    text:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    author:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    post:{
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    }
+},{
+    timestamps: true,
+})
