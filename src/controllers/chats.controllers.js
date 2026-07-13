010a09ebc2af9426ddbc81f41c2e68e6166c590c
Anam029 <anamkhurshid029@gmail.com>
2026-07-13 10:39:45 +0530

    Implement CRUD for Chat functionality

---
+++ src/controllers/chats.controllers.js
@@ -0,0 +1,149 @@
+import { Chat } from "../models/chats.models";
+
+
+
+export async function createChat(req, res) {
+    try {
+        const { sender, receiver, chat } = req.body;
+
+        
+        if (!sender || !receiver || !chat) {
+            return res.status(400).json({
+                success: false,
+                message: "Sender, receiver and chat are required."
+            });
+        }
+
+        
+        const newChat = await Chat.create({
+            sender,
+            receiver,
+            chat
+        })      
+        return res.status(201).json({
+            success: true,
+            message: "Chat created successfully.",
+            data: newChat
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: error.message
+        });
+    }
+}
+
+
+export async function getChat(req, res) {
+    try {
+        const { sender, receiver } = req.query;
+
+        if (!sender || !receiver) {
+            return res.status(400).json({
+                success: false,
+                message: "Sender and Receiver are required"
+            });
+        }
+
+        const chats = await Chat.find({
+            $or: [
+                { sender, receiver },
+                { sender: receiver, receiver: sender }
+            ]
+        })
+        .populate("sender", "username email")
+        .populate("receiver", "username email")
+        .sort({ createdAt: 1 });
+
+        return res.status(200).json({
+            success: true,
+            chats
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export async function updateChat(req, res) {
+    try {
+        const { chatId } = req.params;
+        const { chat } = req.body;
+
+        if (!chatId) {
+            return res.status(400).json({
+                success: false,
+                message: "Chat ID is required"
+            });
+        }
+
+        if (!chat) {
+            return res.status(400).json({
+                success: false,
+                message: "Chat message is required"
+            });
+        }
+
+        const updatedChat = await Chat.findByIdAndUpdate(
+            chatId,
+            { chat },
+            { new: true }
+        );
+
+        if (!updatedChat) {
+            return res.status(404).json({
+                success: false,
+                message: "Chat not found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Chat updated successfully",
+            updatedChat
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export function deleteChat(req,res){
+    try {
+        const {chatId} = req.params
+        if (!chatId) {
+            return res.status(400).json({
+                success: false,
+                message: "Chat ID is required"
+            });
+        }
+
+        const deletedChat = await Chat.findByIdAndDelete(chatId);
+
+        if (!deletedChat) {
+            return res.status(404).json({
+                success: false,
+                message: "Chat not found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Chat deleted successfully"
+        });
+
+        
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
--- src/models/chats.models.js
+++ src/models/chats.models.js
@@ -1,1 +1,30 @@
-import mongoose from "mongoose";
+import mongoose,{Schema} from "mongoose";
+
+const chatSchema = new Schema({
+    sender:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    receiver:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+        message:{
+            type: String,
+            required: true,
+            trim: true
+
+        },
+        isSeen:{
+            type: Boolean,
+            required: true,
+            default: false
+
+        },
+
+    
+},{timestamps: true})
+
+export const Chat = mongoose.model("Chat",chatSchema)
