af5f442fa132bcc5eafae2a64b06dab0a4cc827c
Anam029 <anamkhurshid029@gmail.com>
2026-07-13 10:47:50 +0530

    Added Chat router

---
+++ src/routes/chats.routes.js
@@ -0,0 +1,9 @@
+import {Router} from "express"
+import {createChat,getChat,updateChat,deleteChat}   from "../controllers/chats.controllers.js"
+
+const router = Router()
+
+router.post("/chat",createChat)
+router.get("/chat",getChat)
+router.patch("/chat/id",updateChat)
+router.delete("/chat/id",deleteChat)
